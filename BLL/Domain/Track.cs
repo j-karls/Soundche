973bb2ad7948a6412e960abd896b3dc2ac4dc7c5
Timo Pedersen <timo@123abc.com>
2020-07-18 23:35:59 +0200

    playlistcontroler

---
+++ BLL/Domain/Track.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Domain
+{
+    class Track
+    {
+        public string Name { get; set; }
+        public string Link { get; set; }
+        public int Length { get; set; }
+    }
+}
