378be524c652a322de7270285f5ad027cbcddae1
Jonathan Karlsson <Karlzone0@gmail.com>
2020-07-28 15:19:17 +0200

    Installed sqlite, ran out of time to do more :(

--- BLL/BLL/SQLiteDbController.cs
+++ BLL/BLL/SQLiteDbController.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Domain;
+using Microsoft.Data.Sqlite;
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
@@ -17,33 +18,31 @@ public class SQLiteDbController : IDatabaseController
         // https://docs.microsoft.com/en-us/dotnet/standard/data/sqlite/?tabs=netcore-cli
         // make new documentbased db if none exists, establish connection
 
+        // IF FILE NOT EXISTS
 
-        /*
-         using (var connection = new SqliteConnection("Data Source=hello.db"))
-        {
-            connection.Open();
+        /*using (var connection = new SqliteConnection($"Data Source={DbPath}")) // it destroys the sqliteconnect obj after?
+       {
+           connection.Open();
 
-            var command = connection.CreateCommand();
-            command.CommandText =
-            @"
-                SELECT name
-                FROM user
-                WHERE id = $id
-            ";
-            command.Parameters.AddWithValue("$id", id);
+           var command = connection.CreateCommand();
+           command.CommandText =
+           @"
+               SELECT name
+               FROM user
+               WHERE id = $id
+           ";
+           command.Parameters.AddWithValue("$id", id);
 
-            using (var reader = command.ExecuteReader())
-            {
-                while (reader.Read())
-                {
-                    var name = reader.GetString(0);
+           using (var reader = command.ExecuteReader())
+           {
+               while (reader.Read())
+               {
+                   var name = reader.GetString(0);
 
-                    Console.WriteLine($"Hello, {name}!");
-                }
-            }
-        }
-        */
-
+                   Console.WriteLine($"Hello, {name}!");
+               }
+           }
+       }*/
         throw new NotImplementedException();
     }
 
