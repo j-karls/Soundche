67af1ba8be7f45e3a233de5b11c6af92ac24d594
Jonathan Karlsson <Karlzone0@gmail.com>
2020-08-09 19:41:24 +0200

    Cleaned up playlistcontroller

--- BLL/BLL/PlaylistController.cs
+++ BLL/BLL/PlaylistController.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Domain;
 using Core.Domain.SongQueueMethod;
+using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,26 +10,40 @@ namespace Core.BLL
 {
     public class PlaylistController
     {
-        public SongQueueMethodEnum SongQueueType { get; set; }
-
-        //private func songfunc
-        
+        public SongQueueMethodEnum SongQueueType { get; private set; }
+        private IQueueMethod _queueFunc;
         public List<Playlist> ActivePlaylists { get; private set; }
 
-
-
-
-        public Track GetNewTrack() // Get new track from a playlist
+        public PlaylistController(List<Playlist> playlists, SongQueueMethodEnum queueType)
         {
-            // Put this into some "collected playlists manager" class
-            return new Track("Yir Boi", "urlboi", 0, 10);
+            ActivePlaylists = playlists;
+            SwitchSongQueueMethod(queueType);
         }
+
+        public void SwitchSongQueueMethod(SongQueueMethodEnum queueType)
+        {
+            switch (queueType)
+            {
+                case SongQueueMethodEnum.Randomize: _queueFunc = new QueueRandomize(ActivePlaylists);
+                    break;
+                case SongQueueMethodEnum.RoundRobin: _queueFunc = new QueueRoundRobin(ActivePlaylists);
+                    break;
+                case SongQueueMethodEnum.WeightedRoundRobin: _queueFunc = new QueueWeightedRoundRobin(ActivePlaylists);
+                    break;
+                default: throw new NotImplementedException();
+            }
+        }
+
+        public Track GetNextTrack() => _queueFunc.Next();
 
         public void AddPlaylist(Playlist playlist)
         {
+            // Add a playlist to the total playback and reload our queue method
+            ActivePlaylists.Add(playlist);
+            SwitchSongQueueMethod(SongQueueType);
 
-            // Add a playlist to the total playback
-            throw new NotImplementedException();
+            // TODO: This likely has some problems regarding which song we got to. So we should probably like 
+            // to add the playlist gracefully to an existing songqueuemethod somehow. Probably add that method to the interface
         }
     }
 }
