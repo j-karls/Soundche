d0a26e872ee3e7cef68c57fcddc9119418906381
emilahoe <emilahoe@gmail.com>
2020-08-14 18:19:26 +0200

    Added mvc project

---
+++ Soundche - (Final) Den sande scooter v2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Soundche____Final__Den_sande_scooter_v2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
